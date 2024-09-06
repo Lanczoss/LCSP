036292dfcbeaefb3a1c65c726f552a1858fd99ba
nihility <hnihility1013@163.com>
2024-09-06 22:07:22 +0800

    在2024/9/6在server文件夹下修改了log.c为log_config.c,并修改了pool.c

--- server/pool.c
+++ server/pool.c
@@ -5,21 +5,23 @@
 //第一个参数是共用结构体        
 int initThreads(pool_t *pool)
 {
+    // 从配置文件读取线程数量  
+    // TODO:配置文件的测试格式eg:port=8080 err_eg:port = 8080
+    char num[16];  
+    bzero(num, sizeof(num));  
+    getParameter((void *)"thread_num", (void *)num);  
+    
 
-    // 从配置文件读取线程数量
-    char num[4];
-    bzero(num, sizeof(num));
-    getParameter((void *)"num", (void *) num);
-    // 线程数量
-    pool->thread_num = *num;
+    // 将读取的字符串转换为整数  
+    int thread_count = atoi(num);  
+    if (thread_count <= 0) {  
+        fprintf(stderr, "Invalid thread count: %s\n", num);  
+        return -1; // 处理无效的线程数量  
+    }  
+    printf("thread_count %d \n", thread_count);
+    // 线程数量  
+    pool->thread_num = thread_count;  
 
-    // 将读取的字符串转换为整数
-    int thread_count = atoi(num);
-    if(thread_count < 0)
-    {
-        fprintf(stderr, "Invalid thread count %s \n", num);
-        return -1;  //处理无效的线程数量
-    }
     // 初始化队列
     bzero(&pool->q, sizeof(Queue));
 
@@ -31,16 +33,18 @@ int initThreads(pool_t *pool)
 
     // 初始化退出标记位
     pool->exit_flag = 0;    // 0表示正常，1表示要退出
-    // 给pthread_list分配空间
-    pool->pthread_list = (pthread_t *)malloc(thread_count * sizeof(pthread_t));
+     // 给pthread_list分配空间  
+    // TODO:记得释放空间
+    pool->pthread_list = (pthread_t *)malloc(thread_count * sizeof(pthread_t));  
     ERROR_CHECK(pool->pthread_list, NULL, "malloc");
-    
+
     // 创建子线程
-    for(int i=0; i<thread_count; i++)
+    for(int i=0; i<pool->thread_num; i++)
     {
         pthread_create(&pool->pthread_list[i], NULL, threadMain, pool);
     }
-
+    // 释放空间
+    free(pool->pthread_list);
     return 0;
 }
 
