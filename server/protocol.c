8896fb21b950daa0f8414de20edfffdaf6b8063d
su0608 <1844920392@qq.com>
2024-09-08 11:50:53 +0800

    修改客户端split文件

--- server/protocol.c
+++ server/protocol.c
@@ -7,23 +7,23 @@ int analysisProtocol(train_t t, int net_fd)
     switch(t.command)
     {
     case LS:
-        lsCommand(t, net_fd);
+        //lsCommand(t, net_fd);
         break;
     case CD:
         cdCommand(t, net_fd);
         break;
     case PWD:
-        pwdCommand(t, net_fd);
+        //pwdCommand(t, net_fd);
         break;
     case PUTS:
         putsCommand(t, net_fd);
         break;
     case GETS:
-        getsCommand(t, net_fd);
+        //getsCommand(t, net_fd);
         break;
     case REMOVE:
     case RM:
-        rmCommand(t, net_fd);
+        //rmCommand(t, net_fd);
         break;
     default:
         printf("error:default\n");
