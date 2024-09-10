e837cb1ed9649383477aebfef0f3ea740e701c69
aaa <aaa@qq.com>
2024-09-10 10:08:37 +0800

    续传puts

--- client/putsCommand.c
+++ client/putsCommand.c
@@ -11,13 +11,10 @@
 **/
 int putsCommand(train_t t, int socket_fd) {
 
-    //printf("进入客户端没有\n");
-
     // 获取绝对路径
     char client_path[2048] = {0};
     getcwd(client_path, sizeof(client_path));
-
-    //printf("绝对路径%s\n",client_path);
+    printf("绝对路径%s\n",client_path);
 
     // 获取用户路径
     char user_path[256] = {0};
@@ -31,7 +28,7 @@ int putsCommand(train_t t, int socket_fd) {
 //        strcat(client_path, user_path);
 //    }
 
-    //printf("客户端本地路径：%s\n", client_path);
+    printf("客户端本地路径：%s\n", client_path);
 
     // 获取文件名并处理路径
     char path_name[256] = {0};
@@ -39,59 +36,64 @@ int putsCommand(train_t t, int socket_fd) {
         printf("无法提取路径名\n");
         return -1;
     }
-
     if (path_name[0] != '/' && path_name[0] != '.') {
         strcat(client_path, "/");
         strcat(client_path, path_name);
     } else {
         strcpy(client_path, path_name);
     }
-
     // 去掉换行符
     size_t len = strcspn(client_path, "\n");
     if (len < strlen(client_path)) {
         client_path[len] = '\0'; // 将换行符替换为字符串终止符
     }
+    printf("打开文件的路径名:%s\n", client_path);
 
-    //printf("打开文件的路径名:%s\n", client_path);
 
     // 判断上传文件是否存在
-    int open_file_fd = open(client_path, O_RDONLY);
+    int open_file_fd = open(client_path, O_RDWR);
     if (open_file_fd == -1) {
         perror("open file error");
         return -1;
     }
-    //printf("成功打开 %s 下的文件\n", client_path);
+    printf("成功打开%s\n", client_path);
 
-    // 发送结构体
-    ssize_t send_t = send(socket_fd, &t, sizeof(t), MSG_NOSIGNAL);
-    if (send_t == -1) {
-        perror("send train_t error");
-        close(open_file_fd);
-        return -1;
-    }
-
-    //printf("发送的结构体中的字符串: %s\n", t.control_msg);
-
-    // 发送文件
+    //获取文件信息
     struct stat file_stat;
     if (fstat(open_file_fd, &file_stat) == -1) {
         perror("fstat error");
         close(open_file_fd);
         return -1;
     }
+    // 发送结构体
+    t.file_length=file_stat.st_size;
+    ssize_t send_t = send(socket_fd, &t, sizeof(t), MSG_NOSIGNAL);
+    if (send_t == -1) {
+        perror("send train_t error");
+        close(open_file_fd);
+        return -1;
+    }
+    printf("发送的结构体中的字符串: %s\n", t.control_msg);
 
-    ssize_t sendfile_t = sendfile(socket_fd, open_file_fd, NULL, file_stat.st_size);
+//等待serve：60行
+    //接收偏移量
+    long offset=0;
+    recv(socket_fd,&offset,sizeof(offset),MSG_WAITALL);
+    printf("发送文件偏移量为:%ld\n",offset);
+
+    //lseek(open_file_fd,offset,SEEK_SET);
+
+    ssize_t sendfile_t=sendfile(socket_fd,open_file_fd,&offset,file_stat.st_size-offset);
     if (sendfile_t == -1) {
         perror("sendfile error");
         close(open_file_fd);
         return -1;
     }
 
-    printf("文件发送完毕！\n");
-
-    // 释放资源
+    printf("所发送字节大小%ld\n",sendfile_t);
+    printf("文件成功发送!\n");
     close(open_file_fd);
+
     return 0;
 }
 
--- server/putsCommand.c
+++ server/putsCommand.c
@@ -10,10 +10,11 @@
   ******************************************************************************
 **/
 int putsCommand(train_t t, int net_fd) {
+
     // 获取服务端路径
     char serve_path[256] = {0};
     snprintf(serve_path, sizeof(serve_path), "%s", BASE_PATH);
-    printf("Base路径: %s\n", serve_path);
+    //printf("Base路径: %s\n", serve_path);
 
     // 获取用户路径
     char user_path[256] = {0};
@@ -24,13 +25,14 @@ int putsCommand(train_t t, int net_fd) {
         //printf("无需拼接服务端本地路径: %s\n", serve_path);
     }
 
+    //printf("服务端下载的路径:%s\n",serve_path);
+
     // 获取文件名
     char path_temp[256] = {0};
     if (extractParameters(t.control_msg, 2, path_temp, sizeof(path_temp)) != 0) {
         fprintf(stderr, "Failed to extract file path\n");
         return -1;
     }
-
     char path_name[256] = {0};
     if (path_temp[0] == '/' || path_temp[0] == '.') {
         if (extractFilename(path_temp, path_name, sizeof(path_name)) != 0) {
@@ -40,52 +42,116 @@ int putsCommand(train_t t, int net_fd) {
     } else {
         strncpy(path_name, path_temp, sizeof(path_name) - 1);
     }
-
     // 拼接完整路径
-//    strncat(serve_path, "/", sizeof(serve_path) - strlen(serve_path) - 1);
     strncat(serve_path, path_name, sizeof(serve_path) - strlen(serve_path) - 1);
-
     // 去掉换行符
     size_t len = strcspn(serve_path, "\n");
     serve_path[len] = '\0'; // 确保以 '\0' 结尾
+    printf("打开文件的路径名: %s\n", serve_path);
+    printf("文件路径名长度%lu\n",strlen(serve_path));
 
-    //printf("打开文件的路径名: %s\n", serve_path);
+    //接收结构体
+    train_t train;
+    int ret=recv(net_fd,&train,sizeof(train),MSG_WAITALL);
+    ERROR_CHECK(ret,-1,"recv train");
+    printf("train文件长度%d和字符内容%s\n",train.file_length,train.control_msg);
+
 
     // 新建文件
-    int open_file_fd = open(serve_path, O_WRONLY | O_CREAT | O_TRUNC, 0666);
-    if (open_file_fd < 0) {
-        perror("open new file");
-        return -1;
-    }
+    int open_file_fd = open(serve_path, O_WRONLY);
+    if(open_file_fd==-1){
+        printf("第一次上传文件\n");
+        //给客户端发送需要上传偏移量
+        long offset=0;
+        send(net_fd,&offset,sizeof(offset),MSG_NOSIGNAL);
+        printf("偏移量%ld\n",offset);
 
-    // 接收文件
-    char buf[1024] = {0};
-    ssize_t recv_num;
-    while (1) {
-        recv_num = recv(net_fd, buf, sizeof(buf), MSG_DONTWAIT);
+        open_file_fd=open(serve_path,O_WRONLY|O_CREAT|O_APPEND,0666);
+        printf("%d,%s\n",open_file_fd,path_name);
+        ERROR_CHECK(open_file_fd,-1,"open");
+        printf("文件创建成功\n");
 
-        if (recv_num < 0) {
-            perror("recv failed");
-            break;
-        } else if (recv_num == 0) {
-            //printf("客户端关闭了连接\n");
-            break;
+        //接收文件
+        char buf[1024] = {0};
+        ssize_t recv_num;
+        ssize_t count=0;
+        while (1) {
+            recv_num = recv(net_fd, buf, sizeof(buf), MSG_DONTWAIT);
+            printf("所接收到的内容为%s\n",buf);
+            if (recv_num < 0&&train.file_length==count) {
+                perror("recv failed");
+                break;
+            } else if (recv_num == 0) {
+                printf("客户端关闭了连接\n");
+                break;
+            }
+
+            printf("接收到 %ld 字节\n", recv_num);
+            write(open_file_fd, buf, recv_num);
+
+            if(recv_num!=-1){
+                count+=recv_num;
+            }
+
+//            ssize_t write_file_num = write(open_file_fd, buf, recv_num);
+//            if (write_file_num < 0) {
+//                perror("write failed");
+//                close(open_file_fd);
+//                return -1;
+//            }
         }
+    }else{
+        //文件存在
+        printf("已有文件\n");
 
-        //printf("接收到 %ld 字节\n", recv_num);
-        ssize_t write_file_num = write(open_file_fd, buf, recv_num);
-        if (write_file_num < 0) {
-            perror("write failed");
-            close(open_file_fd);
-            return -1;
+        struct stat s;
+        fstat(open_file_fd,&s);
+        if(train.file_length>s.st_size){
+            //s现在文件大小，train总文件大小
+
+            //客户端文件长度大于当前文件长度
+//            long offset=s.st_size-t.file_length;
+            long offset=train.file_length-s.st_size;
+            send(net_fd,&offset,sizeof(offset),MSG_NOSIGNAL);
+
+            lseek(open_file_fd,offset,SEEK_SET);
+
+            //接收文件
+            char buf[1024] = {0};
+            ssize_t recv_num;
+            ssize_t count=0;
+            while (1) {
+                recv_num = recv(net_fd, buf, sizeof(buf), MSG_DONTWAIT);
+                printf("所接收到的内容为%s\n",buf);
+                if (recv_num < 0&&train.file_length-offset==count) {
+                    perror("recv failed");
+                    break;
+                } else if (recv_num == 0) {
+                    printf("客户端关闭了连接\n");
+                    break;
+                }
+
+                printf("接收到 %ld 字节\n", recv_num);
+                write(open_file_fd, buf, recv_num);
+
+                if(recv_num!=-1){
+                    count+=recv_num;
+                }
+//                ssize_t write_file_num = write(open_file_fd, buf, recv_num);
+//                if (write_file_num < 0) {
+//                    perror("write failed");
+//                    close(open_file_fd);
+//                    return -1;
+//                }
+            }
+
         }
     }
-
 
 
     printf("服务器成功收到文件: %s\n", serve_path);
-
     close(open_file_fd);
+
     return 0;
 }
 
