98ffe64ee8614eaec10f08c96e653448a9c4eaaa
whao777 <766475567@qq.com>
2024-09-11 13:42:52 +0800

    添加数据库后客户端ls命令的修改

--- client/lsCommand.c
+++ client/lsCommand.c
@@ -1,110 +1,211 @@
 #include "header.h"
 
 
-int getFileName(char *src, char *file_name){
-    char *str;
+//从train自定义协议中取得参数
+//第一个参数指自定义协议
+//第二个参数指需要第几个参数
+//第三个参数指字符数组缓冲区
+int splitParameter(train_t t, int num, char *buf){
 
-    // 查找最后一个斜杠的位置
-    str = strrchr(src, '/');
+    //当前t.control_msg存放信息：zs/ 参数1 参数2
 
-    // 如果没有找到斜杠，返回 src 本身作为文件名
-    if (str == NULL) {
-        str = src;
-    }else{
-        // gets后是目录加文件名
-        str++;
-        if(str[0] == 32){
-            // gets后直接是文件名
-            str++;
-        }
+    //取出t.control_msg中的信息，并且要保持里面的信息内容不变
+    char str[1024] = { 0 };
+
+    //放入str中，在str中取出数据
+    strcpy(str,t.control_msg);
+
+    char *parma;
+    //取出路径
+    parma = strtok(str," ");
+
+    //取出参数
+    for(int i = 0;i < num;i++){
+        parma = strtok(NULL, " ");
+        ERROR_CHECK(parma, NULL, "strtok");
     }
 
-    // 确保 file_name 有足够的空间
-    strncpy(file_name, str, strlen(str));
+    strcpy(buf,parma);
 
     return 0;
 }
 
-
-
-int removeLineBreak(char *real_path){
-    // 找到换行符的位置
-    size_t len = strcspn(real_path, "\n");
-
-    // 如果找到了换行符，len 小于路径名的长度
-    if (len < strlen(real_path)) {
-        real_path[len] = '\0'; // 将换行符替换为字符串终止符
-    }
-
-    return 0;
-}
-
-
-int getsCommand(train_t t, int socket_fd){
-
-
-    // 接收是否返回信息，判断文件是否可以下载
-    recv(socket_fd, &t, sizeof(train_t), MSG_WAITALL);
-
-
-    // TODO 对返回信息判断
-    if(t.error_flag == 1){
-        // 服务端文件不存在
-        printf("文件不存在,下载失败！\n");
-        return -1;
-    }
-    // 到这里可下载
-    // 获取文件名
-    char file_name[1024] = { 0 };
-    getFileName(t.control_msg, file_name);
-    removeLineBreak(file_name);
-
-
-    // 创建文件
-    int file_fd = open(file_name, O_RDWR | O_CREAT, 0666);
-
-    // 接收文件大小
-    off_t file_size = 0;
-    recv(socket_fd, &file_size, sizeof(off_t), MSG_WAITALL);
-
-    // mmap之前修改文件大小
-    ftruncate(file_fd, file_size);
-
-    // 接收数据
-    void *p = mmap(NULL, file_size, PROT_READ | PROT_WRITE, MAP_SHARED, file_fd, 0);
-
-    recv(socket_fd, p, file_size, MSG_WAITALL);
-
-    munmap(p, file_size);
-
-    close(file_fd);
-    
-    printf("文件下载成功\n");
-
-    return 0;
-}
 
 
 int lsCommand(train_t t, int socket_fd){
+    // 参数大于1，等待服务端发送的错误信息
+    if(t.parameter_num > 1){
+        recv(socket_fd, &t, sizeof(train_t), MSG_WAITALL);
+        if(t.error_flag == 1){
+            printf("参数个数不合法，请输入合法参数\n");
+        }
+        return 0;
+    }
 
-    // 先接收错误信息,错误处理
+    // 参数个数为0，ls查表一定成功，接收结果
+    if(t.parameter_num == 0){
+        // 为了兼容下面
+        // 服务端多send了一次，多recv一次
+        recv(socket_fd, &t, sizeof(t), MSG_WAITALL);
+
+        // 先接收是否有错误信息
+        recv(socket_fd, &t, sizeof(train_t), MSG_WAITALL);
+
+        // 错误标志如果为3，说明查表成功
+        // 但未查到数据
+        // 即当前目录下无内容
+        // 打印换行
+        if(t.error_flag == 3){
+            printf("当前目录下无内容\n");
+            return 0;
+        }
+        // 查到数据进行打印
+        int file_size;
+        char buffer[1024] = { 0 };
+
+        // 先接文件大小，再接内容
+        recv(socket_fd, &file_size, sizeof(int), MSG_WAITALL);
+
+        recv(socket_fd, buffer, file_size, MSG_WAITALL);
+
+        printf("%s\n",buffer);
+
+        return 0;
+    }
+
+    // 到这儿说明ls后为1个参
+    char path[256] = { 0 };
+    char para[256] = { 0 };
+
+    // 获得客户端目前路径
+    strncpy(path, t.control_msg, t.path_length);
+    // 获得参数
+    splitParameter(t, 1, para);
+
+    // 如果参数不以斜杠开头,添加一个斜杠
+    if(para[0] != '/'){
+        size_t len = strlen(para);
+        memmove(para + 1, para, len + 1);
+        para[0] = '/';
+    }
+
+    // 客户端目前路径和参数一样
+    // 相当于ls无参
+    if(strcmp(path, para) == 0){
+        // 为了兼容下面，服务端多发了一次
+        // 多recv一次
+        recv(socket_fd, &t, sizeof(train_t), MSG_WAITALL);
+
+        // 先接收是否有错误信息
+        recv(socket_fd, &t, sizeof(train_t), MSG_WAITALL);
+
+        // 错误标志如果为3，说明查表成功
+        // 但未查到数据
+        // 即当前目录下无内容
+        // 打印换行
+        if(t.error_flag == 3){
+            printf("当前目录下无内容\n");
+            return 0;
+        }
+        // 查到数据进行打印
+        int file_size;
+        char buffer[1024] = { 0 };
+
+        // 先接文件大小，再接内容
+        recv(socket_fd, &file_size, sizeof(int), MSG_WAITALL);
+
+        recv(socket_fd, buffer, file_size, MSG_WAITALL);
+
+        printf("%s\n",buffer);
+
+        return 0;
+
+    }
+
+    // 检查参数是否是以当前路径开头
+    if(strncmp(para, path, t.path_length) == 0){
+        // 这里需检查错误标志是否为2
+        // 表中没有以当前参数为路径的记录
+        recv(socket_fd, &t, sizeof(train_t), MSG_WAITALL);
+
+        if(t.error_flag == 2){
+            // 说明当前目录下无该目录或文件
+            printf("当前目录下无该目录或文件\n");
+            return 0;
+        }
+
+        // 先接收是否有错误信息
+        recv(socket_fd, &t, sizeof(train_t), MSG_WAITALL);
+
+        // 错误标志如果为3，说明查表成功
+        // 但未查到数据
+        // 即当前目录下无内容
+        // 打印换行
+        if(t.error_flag == 3){
+            printf("当前目录下无内容\n");
+            return 0;
+        }
+        // 查到数据进行打印
+        int file_size;
+        char buffer[1024] = { 0 };
+
+        // 先接文件大小，再接内容
+        recv(socket_fd, &file_size, sizeof(int), MSG_WAITALL);
+
+        recv(socket_fd, buffer, file_size, MSG_WAITALL);
+
+        printf("%s\n",buffer);
+
+        return 0;
+
+    }
+
+    if(strlen(para) == 1 && para[0] == '/'){
+        recv(socket_fd, &t, sizeof(train_t), MSG_WAITALL);
+        if(t.error_flag == 4){
+            printf("非法参数\n");
+        }
+        return 0;
+    }
+
+
+    // 到这里就是参数不以路径开头，且不相同
+
+
+    // 这里需检查错误标志是否为2
+    // 表中没有以当前参数为路径的记录
     recv(socket_fd, &t, sizeof(train_t), MSG_WAITALL);
 
-
-    if(t.error_flag == 1){
-        // 错误处理
-        printf("没有那个文件或目录\n");
-        return -1;
+    if(t.error_flag == 2){
+        // 说明当前目录下无该目录或文件
+        printf("当前目录下无该目录或文件\n");
+        return 0;
     }
-    off_t size;
-    // 所接收文件大小
-    recv(socket_fd, &size, sizeof(off_t), MSG_WAITALL);
 
+
+    // 先接收是否有错误信息
+    recv(socket_fd, &t, sizeof(train_t), MSG_WAITALL);
+
+    // 错误标志如果为3，说明查表成功
+    // 但未查到数据
+    // 即当前目录下无内容
+    // 打印换行
+    if(t.error_flag == 3){
+        printf("当前目录下无内容\n");
+        return 0;
+    }
+    // 查到数据进行打印
+    int file_size;
     char buffer[1024] = { 0 };
-    // 服务端ls命令结果
-    recv(socket_fd, buffer, size, MSG_WAITALL);
 
-    printf("%s",buffer);
+    // 先接文件大小，再接内容
+    recv(socket_fd, &file_size, sizeof(int), MSG_WAITALL);
+
+    recv(socket_fd, buffer, file_size, MSG_WAITALL);
+
+    printf("%s\n",buffer);
+
+    return 0;
 
     return 0;
 }
