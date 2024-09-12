c6acdf58717ef994fa943eac2a0dc681b01a0ac7
su0608 <1844920392@qq.com>
2024-09-12 11:56:43 +0800

    修复在文件夹下创建多个文件夹pid问题

--- server/insertDir.c
+++ server/insertDir.c
@@ -2,36 +2,43 @@
 #include <mysql/mysql.h>
 
 int insertDir(train_t t, char * real_path, char * dirname,MYSQL*mysql){
-    
+
     MYSQL_RES *res;
     MYSQL_ROW row;
-    
+    // 检查路径的最后一个字符是否是 '/'
+    size_t len = strlen(real_path);
+    if (len > 0 && real_path[len - 1] == '/') {
+        // 将末尾的 '/' 替换为字符串结束符 '\0'
+        real_path[len - 1] = '\0';
+    }
     //获取pid
     int pid = 0;
     char sql_pid[1024] = { 0 };
     snprintf(sql_pid, sizeof(sql_pid), 
-              "SELECT id FROM files WHERE file_path = '%s' AND uid = %d", real_path,t.uid);
+             "SELECT id FROM files WHERE file_path = '%s' AND uid = %d", real_path,t.uid);
     mysql_query(mysql,sql_pid);
-    
+
     res = mysql_store_result(mysql);
-    
-    while((row = mysql_fetch_row(res))){
-        for(int i = 0;i < mysql_num_fields(res);i++){        //获取pid
-            pid = atoi(row[0]);
+    if (res) {
+        if ((row = mysql_fetch_row(res))) {
+            pid = atoi(row[0]);  // 获取pid
         }
+        mysql_free_result(res);  // 释放查询结果
+    } else {
+        fprintf(stderr, "获取pid查询结果失败: %s\n", mysql_error(mysql));
+        return -1;
     }
-    printf("%d\n",pid);
     //判断路径是否为跟或者目录
     //以路径结尾是否为"/"区分
     if(real_path[strlen(real_path) - 1] != '/'){
-       //末尾添加'/' 
-       strcat(real_path, "/");
+        //末尾添加'/' 
+        strcat(real_path, "/");
     }
 
     char file_path[1024] = { 0 };
     strcpy(file_path, real_path);
     strcat(file_path,dirname);
-    
+
     //设置字符编码
     mysql_set_character_set(mysql, "utf8mb4");
 
@@ -40,7 +47,7 @@ int insertDir(train_t t, char * real_path, char * dirname,MYSQL*mysql){
     snprintf(check_sql, sizeof(check_sql),
              "SELECT id FROM files WHERE uid = %d AND file_path = '%s' AND delete_flag = 0",
              t.uid, file_path);
-    
+
     if (mysql_query(mysql, check_sql)) {
         fprintf(stderr, "检测文件夹存在性失败: %s\n", mysql_error(mysql));
         return -1;
@@ -69,14 +76,13 @@ int insertDir(train_t t, char * real_path, char * dirname,MYSQL*mysql){
              "VALUES ('%s', %d, %d, '%s', 1, now(),now())",
              dirname, t.uid, pid, file_path);
 
-    printf("#%s#\n",sql);
     LOG_INFO(sql);
-    
+
     if(mysql_query(mysql,sql)){
         printf("%s\n",mysql_error(mysql));
         return -1;
     }
-    
+
     res = mysql_store_result(mysql);
 
     mysql_free_result(res);
