412f54593a73709169ea7ea945b7d0ede4b4e597
su0608 <1844920392@qq.com>
2024-09-12 20:56:19 +0800

    修复多个文件删除的bug

--- client/mkdirCommand.c
+++ client/mkdirCommand.c
@@ -5,8 +5,12 @@ int mkdirCommand(train_t *t, int socket_fd){
 
     //send(socket_fd,&t,sizeof(train_t),MSG_NOSIGNAL);
     printf("正在创建文件!\n");
-    char message[1024] = { 0 };
-    recv(socket_fd,message,sizeof(message),0);
-    printf("%s\n",message);
+    int num = t->parameter_num;
+    for(int i = 0;i < num ;i++){
+
+        char message[1024] = { 0 };
+        recv(socket_fd,message,sizeof(message),0);
+        printf("%s\n",message);
+    }
     return 0;
 }
